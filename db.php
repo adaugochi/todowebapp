955760c3a9ba0ac7aba6a59ba7b9e8374ddcca71
Adaa <adaamgbede@gmail.com>
2020-08-02 22:49:07 +0100

    add application environment variable

--- db.php
+++ db.php
@@ -1,7 +1,6 @@
 <?php
 
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require_once __DIR__ . '\vendor\autoload.php';
+if (getenv('APP_ENV') !== 'production') {
 
     $repository = Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
         ->addAdapter(Dotenv\Repository\Adapter\EnvConstAdapter::class)
